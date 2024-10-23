6a890af6ac28facd85a5f47e7ff393675109af44
Imaruoka <komasanasahi@gmail.com>
2024-10-23 16:34:58 +0900

    ブランチ移動したいのでコミット

--- src/Wrapper/APIWrapper/spotify_web_api_handler.py
+++ src/Wrapper/APIWrapper/spotify_web_api_handler.py
@@ -21,14 +21,9 @@ class SpotifyWebAPIHandler:
        if(client_credentials_manager == True ):
          return ("success")
      
-    def search_track_spotify_web_api(spotify,sometext):
-         search_str = sometext   #chatGPTからのテキストを挿入する#
-         results = spotify.search(q=search_str, limit=20)
+    def search_track_spotify_web_api(spotify=spotipy.Spotify):
+         #search_str = sometext #chatGPTからのテキストを挿入する#
+         results = spotify.search(q="冬の景色", limit=20, type='track', market ='JP')  #qにはキーワードを入れる。
          for idx, track in enumerate(results['tracks']['items']):
             print(f"{idx:03d}\t{track['name']}\t")
-       
-         
 
-       
-    #    sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=client_id, client_secret=client_secret))
-    
