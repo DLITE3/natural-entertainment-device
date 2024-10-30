a9b8a40f36cf96c1150158fb4d18a6fc9446df8f
imaruoka <b1022038@fun.ac.jp>
2024-10-30 16:05:48 +0900

    NotFoundErrorの解消

--- tests/Wrapper/APIWrapper/SpotifyAPIHandle/test_get_api.py
+++ tests/Wrapper/APIWrapper/SpotifyAPIHandle/test_get_api.py
@@ -4,7 +4,7 @@ import pytest
 import os
 import sys
 sys.path.append(os.getcwd())
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../APIWrapper')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../../')))
 from  src.Wrapper.APIWrapper.spotify_web_api_handler import * #テストするファイルを参照
 
 #インスタンスを作成
@@ -14,7 +14,9 @@ client_secret= handler.get_client_secret()
 	
  
 def test_getAPI():
+
     #  assert SpotifyWebAPIHandler.connected_to_spotify_web_api(\
     #     client_id , client_secret ) == (SpotifyWebAPIHandler.get_client_id, SpotifyWebAPIHandler.get_client_secret)
     assert handler.connected_to_spotify_web_api(client_id,client_secret) == ("success")
-     
+    assert handler.connected_to_spotify_web_api(client_id, client_secret) == ("failure")
+    
