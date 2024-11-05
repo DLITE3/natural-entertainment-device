ff0b35be39a528885e367a014f6b3e519f90c10a
kCat-fun <kcat.fun.stu@gmail.com>
2024-11-05 23:33:19 +0900

    テストOK

--- tests/Wrapper/DeviceWrapper/test_display_is_active.py
+++ tests/Wrapper/DeviceWrapper/test_display_is_active.py
@@ -2,25 +2,25 @@ from pathlib import Path
 import sys
 
 sys.path.append(str(Path(__file__).resolve().parent.parent.parent.parent))
-from src.Wrapper.DeviceWrapper import DeviceWrapper
+from src.Wrapper.DeviceWrapper import DisplayWrapper
 
-def test_display_mock_connected_true(mocker):
+def test_display_mock_connected_true():
     """
     is_connected関数の返す値がTrueになるようにする。
     """
     # ディスプレイオブジェクトをモック
-    mock_display = DeviceWrapper()
+    mock_display = DisplayWrapper()
     mock_display.connected = True
 
     # テスト実行
     assert mock_display.is_connected() == True
 
-def test_display_mock_connected_false(mocker):
+def test_display_mock_connected_false():
     """
     is_connected関数の返す値がFalseになるようにする。
     """
     # ディスプレイオブジェクトをモック
-    mock_display = DeviceWrapper()
+    mock_display = DisplayWrapper()
     mock_display.connected = False
 
     # テスト実行
