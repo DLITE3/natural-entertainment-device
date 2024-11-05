75b569324178f3c929557ef02afaf4f0df3a63b3
kCat-fun <kcat.fun.stu@gmail.com>
2024-11-05 23:15:01 +0900

    テスト前

--- OperationCheck/display_wrapper_operation_check.py
+++ OperationCheck/display_wrapper_operation_check.py
@@ -2,10 +2,12 @@ from pathlib import Path
 import sys
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 
-from src.Wrapper.DeviceWrapper import *
+from src.Wrapper.DeviceWrapper.display_wrapper import DisplayWrapper
 
 def main():
     display: DisplayWrapper = DisplayWrapper()
+    print(display.is_active())
+    display.stop()
 
 if __name__ == "__main__":
     main()
--- src/Wrapper/DeviceWrapper/display_wrapper.py
+++ src/Wrapper/DeviceWrapper/display_wrapper.py
@@ -8,7 +8,10 @@ import board
 from PIL import Image
 
 
-class LcdWrapper:
+class DisplayWrapper:
+    # 成城接続時のSPIaddress
+    CONNECT_OK: int = 548466237520
+
     # コンストラクタ
     def __init__(self):
         print("LcdWrapper:__init__")
@@ -18,11 +21,11 @@ class LcdWrapper:
         rst_pin = DigitalInOut(board.D24)
 
         # Set up SPI bus
-        spi = SPI(clock=board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+        self.spi = SPI(clock=board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 
         # Create the ILI9341 display:
         self.disp = ILI9341(
-            spi,
+            self.spi,
             cs=cs_pin, dc=dc_pin, rst=rst_pin,
             width=240, height=320,
             rotation=90,
@@ -32,8 +35,9 @@ class LcdWrapper:
         # Define image size (320x240, rotated)
         self.IMAGE_SIZE = (self.disp.height, self.disp.width)
 
-    def is_active(self):
-        return self.disp
+    def is_active(self) -> bool:
+        print(id(self.disp))
+        return id(self.spi) == self.CONNECT_OK
 
     def show_image(self, image):
         if image is None:
@@ -47,4 +51,3 @@ class LcdWrapper:
 
     def stop(self):
         self.disp.fill(0)
-        self.disp.show()
