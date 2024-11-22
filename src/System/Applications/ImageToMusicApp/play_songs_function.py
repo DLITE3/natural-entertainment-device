2a44fc374b9fb7bd55892a1f0a2b22ff0b579fec
kCat <kcat.fun.stu@gmail.com>
2024-11-22 15:19:53 +0900

    分離した

---
+++ src/System/Applications/ImageToMusicApp/play_songs_function.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+import sys
+sys.path.append(str(Path(__file__).resolve().parent.parent.parent.parent.parent))
+
+from Wrapper import *
+import cv2
+import time
+
+class PlaySongsFunction:
+    def __init__(self) -> None:
+        self.image_to_song_api = ImageToSongAPIWrapper()
+
+    def select_song(self, image: cv2.MatLike):
+        songs = self.image_to_song_api.image_to_song(image)
+        song_id = None
+        for song in songs["song_list"]:
+            print(song)
+            song_id = self.spotify_web_api.search_track(song)
+            time.sleep(0.5)
+            if song_id != None:
+                print("song: " + song)
+                break
+        return song_id
+    
+    def play_song(self, song_id, serch_device_name: str) -> None:
+        if song_id != None:
+            try:
+                devices = self.spotify_web_api.get_devices()
+                smart_phone_device_id = self.spotify_web_api.search_device(serch_device_name, devices)
+                self.device_id = smart_phone_device_id
+                print(serch_device_name + " id: " + smart_phone_device_id)
+                if smart_phone_device_id:
+                    self.spotify_web_api.play_on_device(song_id, smart_phone_device_id)
+            except Exception as e:
+                print(e)
+                self.spotify_web_api.stop_playback(smart_phone_device_id)
+
+    def stop(self):
+        self.spotify_web_api.stop_playback(self.device_id)
--- src/System/Applications/__init__.py
+++ src/System/Applications/__init__.py
@@ -1,2 +1,2 @@
 from System.Applications.app import *
-from System.Applications.image_to_music_app import *
+from System.Applications.ImageToMusicApp import *
