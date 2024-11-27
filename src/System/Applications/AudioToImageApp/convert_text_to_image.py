6adea58f1b7727133ca70dc076b34610f50ab575
imaruoka <b1022038@fun.ac.jp>
2024-11-27 13:56:57 +0900

    画像生成の機能実装

--- src/System/Applications/AudioToImageApp/convert_text_to_image.py
+++ src/System/Applications/AudioToImageApp/convert_text_to_image.py
@@ -1,9 +1,30 @@
 import sys
 import os
+import base64
 sys.path.append(os.getcwd())
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../../')))
 from src.Wrapper.APIWrapper.stable_diffusion_api_wrapper import StableDiffusionWrapper
 
 class ConvertTexrToImage:
-    def none():
-     return None
+     def __init__(self):
+          self.api_handler = StableDiffusionWrapper()
+
+
+          
+     def send_to_stable_diffusion( self , response):
+         generate = self.api_handler.post_data(self, response)
+         base64_image = generate.get("image")
+         if base64_image:
+                # Base64文字列をバイナリデータに変換
+                image_data = base64.b64decode(base64_image)
+
+                # 画像ファイルとして保存
+                with open("output_image.jpeg", "wb") as f:
+                    f.write(image_data)
+                print("画像が保存されました: output_image.jpeg")
+         else:
+                print("画像データがレスポンスに含まれていません。")
+
+         
+         
+        
--- src/Wrapper/APIWrapper/stable_diffusion_api_wrapper.py
+++ src/Wrapper/APIWrapper/stable_diffusion_api_wrapper.py
@@ -7,7 +7,7 @@ import os
 load_dotenv()
 
 class StableDiffusionWrapper:
-    def __init__(self, ):
+    def __init__(self ):
         self.api_url = os.getenv("STABLE_DIFFUSION_API_URL")
         if not self.api_url:
             raise ValueError("STABLE_DIFFUSION_API_URL が設定されていません。")
@@ -25,22 +25,11 @@ class StableDiffusionWrapper:
             print(f"Error decoding Base64 string: {e}")
             raise
 
-    def generate_image(self, prompt: str) -> Image.Image:
+    def post_data(self, prompt: str) -> Image.Image:
         """
         Stable Diffusion APIにプロンプトを送信して画像を生成する
         """
         data = {"query": prompt}
-        try:
-            response = requests.post(self.api_url, json=data)
-            response.raise_for_status()
-            response_data = response.json()
-        except requests.RequestException as e:
-            print(f"Error sending request to API: {e}")
-            raise
-
-        base64_image = response_data.get("image")
-        if base64_image:
-            return self.base64_to_image(base64_image)
-        else:
-            raise ValueError("レスポンスに画像データが含まれていません。")
+        response = requests.post(self.api_url, json=data)
+        return response.json()
 
