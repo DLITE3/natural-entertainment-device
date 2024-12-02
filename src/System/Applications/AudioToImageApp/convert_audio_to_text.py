e05a7dc5ca4383a137624e47c73d06f85227024d
kCat-fun <kcat.fun.stu@gmail.com>
2024-12-03 05:07:07 +0900

    とりあえず修正

--- src/System/Applications/AudioToImageApp/convert_audio_to_text.py
+++ src/System/Applications/AudioToImageApp/convert_audio_to_text.py
@@ -1,20 +1,20 @@
+from pathlib import Path
+import sys
+sys.path.append(str(Path(__file__).resolve().parent.parent.parent.parent))
+
+from src.Wrapper.APIWrapper.openai_api_handler import OpenAIAPIHandler
+
 import librosa
 import numpy as np
-import sys
 import os
-sys.path.append(os.getcwd())
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../../')))
-from src.Wrapper.APIWrapper.openai_api_handler import OpenAIAPIHandler
 import json
 
-
 class ConvertSoundToLanguage:
-    def __init__(self, audio_path):
+    def __init__(self):
         """
         初期化メソッド
         :param audio_path: 解析対象の音声ファイルのパス
         """
-        self.audio_path = audio_path
         self.y = None  # 音声データ
         self.sr = None  # サンプリングレート
         self.spectrogram = None  # スペクトル分布（パワースペクトログラム）
@@ -26,59 +26,65 @@ class ConvertSoundToLanguage:
 
         self.api_handler = OpenAIAPIHandler()
 
-    def load_audio(self):
+    def load_audio(self, audio_path: str) -> None:
         """
         音声データを読み込む
         """
-        self.y, self.sr = librosa.load(self.audio_path, sr=None)
+        y, sr = librosa.load(audio_path, sr=None)
+        return y, sr
 
-    def analyze_features(self):
+    def analyze_features(self, audio_path: str) -> object:
         """
         音響特徴量を計算する
         """
+
+        # 音声データを読み込む
+        _y, _sr = self.load_audio(audio_path)
+
         # スペクトル分布（パワースペクトログラム）
-        S = np.abs(librosa.stft(self.y))**2
+        S = np.abs(librosa.stft(_y))**2
         self.spectrogram = librosa.amplitude_to_db(S, ref=np.max)
 
         # エンベロープ（振幅包絡線）
-        self.envelope = librosa.onset.onset_strength(y=self.y, sr=self.sr)
+        self.envelope = librosa.onset.onset_strength(y=_y, sr=_sr)
 
         # ゼロ交差率
-        self.zero_crossings = librosa.feature.zero_crossing_rate(self.y)[0]
+        self.zero_crossings = librosa.feature.zero_crossing_rate(_y)[0]
 
         # スペクトル重心
-        self.spectral_centroid = librosa.feature.spectral_centroid(y=self.y, sr=self.sr)[0]
+        self.spectral_centroid = librosa.feature.spectral_centroid(y=_y, sr=_sr)[0]
 
         # スペクトルフラットネス
-        self.spectral_flatness = librosa.feature.spectral_flatness(y=self.y)[0]
+        self.spectral_flatness = librosa.feature.spectral_flatness(y=_y)[0]
 
         # エネルギー
         self.energy = np.sum(S, axis=0)
 
         # 特徴量のまとめ
         results = {
-            "スペクトログラムの最大値": np.max(self.spectrogram),
-            "スペクトログラムの最小値": np.min(self.spectrogram),
-            "エンベロープの平均強度": np.mean(self.envelope),
-            "エンベロープの最大強度": np.max(self.envelope),
-            "ゼロ交差率の平均": np.mean(self.zero_crossings),
-            "ゼロ交差率の最大": np.max(self.zero_crossings),
-            "スペクトル重心の平均": np.mean(self.spectral_centroid),
-            "スペクトル重心の最大": np.max(self.spectral_centroid),
-            "スペクトルフラットネスの平均": np.mean(self.spectral_flatness),
-            "スペクトルフラットネスの最大": np.max(self.spectral_flatness),
-            "エネルギーの平均": np.mean(self.energy),
-            "エネルギーの最大": np.max(self.energy),
-            "エネルギーの最小": np.min(self.energy),
+            "Spectrogram Max Value": np.max(self.spectrogram),
+            "Spectrogram Min Value": np.min(self.spectrogram),
+            "Envelope Mean Intensity": np.mean(self.envelope),
+            "Envelope Max Intensity": np.max(self.envelope),
+            "Zero Crossing Rate Mean": np.mean(self.zero_crossings),
+            "Zero Crossing Rate Max": np.max(self.zero_crossings),
+            "Spectral Centroid Mean": np.mean(self.spectral_centroid),
+            "Spectral Centroid Max": np.max(self.spectral_centroid),
+            "Spectral Flatness Mean": np.mean(self.spectral_flatness),
+            "Spectral Flatness Max": np.max(self.spectral_flatness),
+            "Energy Mean": np.mean(self.energy),
+            "Energy Max": np.max(self.energy),
+            "Energy Min": np.min(self.energy),
         }
 
         return results
 
-    def send_to_gpt(self, results):
-        
-       #特徴量を引数とし、GPTへリクエストを送る    
-       response = self.api_handler.post_request(str(results) + "この音からどのような感情を受け取りますか？\
-                                                テキストをそのままstable diffusionに入力したいので、特徴量の名称は言わず、英語で答えてください\
-                                                「this sound」という言葉もいりません。\
-                                                そして、感情を表す文のみをstable diffusionが入力しやすい形で答えてください。")
-       return response.text
+    def send_to_gpt(self, audio_analyzing_data: str) -> str:   
+        #特徴量を引数とし、GPTへリクエストを送る    
+        response = self.api_handler.post_request(
+            str(audio_analyzing_data) + \
+            "These are the results of analyzing the spectrum and energy of recorded environmental sounds. \
+            Please briefly answer how you perceive the sound, using emotional expressions. \
+            Please also take this into account as the results will be input into the stable diffusion prompts to generate the images."
+        )
+        return response.text
